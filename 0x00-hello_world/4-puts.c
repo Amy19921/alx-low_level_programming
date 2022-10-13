6697f451b96b7fdfdf85112918e202ede593d078
Amina <mina.niass456@gmail.com>
2022-10-13 08:12:28 -0700

    Initial Commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
  *
- *  * main - main block
+ *    main - main block
  *
- *   * Return: 0
+ *    Return: 0
  *
- *    */
+ */
 
 int main(void)
 
@@ -14,6 +14,6 @@ int main(void)
 
 		puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+	        return (0);
 
 }
