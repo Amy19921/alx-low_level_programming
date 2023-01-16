4f627fa524ddcc92095d5f44a8a29db3ed346448
Amina <mina.niass456@gmail.com>
2023-01-16 14:37:27 -0800

    Initial Commit

--- 0x18-dynamic_libraries/4-isalpha.c
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -1,19 +1,22 @@
-#include "main.h"
+#include<ctype.h>
 
 /**
- * _isalpha - function to check if c is a letter, lowercase
+ * _isalpha - _isalpha function checks for alphabetic character
  *
- * @c: is the int that will ise for the argument of the function
- * Description: using the _isalpha function
- * this program prints "
- * Return: 0
+ * @c: char type letter
+ *
+ * Return: return 0 on success and 1 on fail
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	int i = isalpha(c);
+
+	if (i > 0)
 	{
 		return (1);
 	}
 	else
+	{
 		return (0);
+	}
 }
