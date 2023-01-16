cc5ff6d00ce8fa031ab62fe36cdb37d398e89b1f
Amina <mina.niass456@gmail.com>
2023-01-16 14:48:49 -0800

    Initial Commit

--- 0x18-dynamic_libraries/calc.c
+++ 0x18-dynamic_libraries/calc.c
@@ -1,53 +1,26 @@
-#include "main.h"
+#include "calc.h"
 
-/**
- * mul - multiplies two numbers
- * @a: first number
- * @b: second number
- * Return: return result
- */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+int sub(int a, int b)
+{
+	return (a + b);
+}
+
 int mul(int a, int b)
 {
 	return (a * b);
 }
 
-/**
- * add - adds two integers
- *
- * @a: first input
- * @b: second input
- *
- * Return: sum of a & b
- */
-int add(int a, int b)
-{
-	int sum = a + b;
-
-	return (sum);
-}
-
-/**
- * sub - subtracts one integer from another
- * @a: first input
- * @b: second input
- *
- * Return: difference of a and b
- */
-int sub(int a, int b)
-{
-	int sub = a - b;
-
-	return (sub);
-}
-
-/**
- * div - divides two integers
- * @a: first input
- * @b: second input
- *
- * Return: return result
- */
 int div(int a, int b)
 {
 	return (a / b);
+}
+
+int mod(int a, int b)
+{
+	return (a % b);
 }
---
+++ 0x18-dynamic_libraries/calc.h
@@ -0,0 +1,10 @@
+#ifndef _OPERATIONS_H
+#define _OPERATIONS_H
+
+int add(int, int);
+int sub(int, int);
+int mul(int, int);
+int div(int, int);
+int mod(int, int);
+
+#endif
