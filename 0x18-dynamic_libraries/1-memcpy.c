ddb4109c071a5deb9dfa27ff7240e884178e0d46
Amina <mina.niass456@gmail.com>
2023-01-16 16:12:38 -0800

    Initial Commit

--- 0x18-dynamic_libraries/1-memcpy.c
+++ 0x18-dynamic_libraries/1-memcpy.c
@@ -1,25 +1,18 @@
 #include "main.h"
-
 /**
- * _memcpy - copy n bytes of memory from source to destination
- * @dest: destination
- * @src: source
- * @n: number of bytes
- * Return: pointer to dest
+ * _memcpy - Function to copy memory area
+ * @dest: destination memory to copy to
+ * @src: source memory to copy from
+ * @n: number of byte
+ * Return: pointer to destination(dest)
  */
-
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int size = n;
+	unsigned int i;
 
-	if (size > 0)
+	for (i = 0; i < n; i++)
 	{
-		int j;
-
-		for (j = 0; j < size; j++)
-		{
-			dest[j] = src[j];
-		}
+		dest[i] = src[i];
 	}
 	return (dest);
 }
